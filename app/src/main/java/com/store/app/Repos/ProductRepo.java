6c501ee7ee179223cf4abf639d399965636d2a1f
Atom Labs <atombaseinc@gmail.com>
2025-10-15 09:45:17 +0530

    D3 Configs

--- app/src/main/java/com/store/app/Repos/ProductRepo.java
+++ app/src/main/java/com/store/app/Repos/ProductRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.store.app.Models.Product;
 
-public interface ProductRepo extends  JpaRepository<Product, Long>{
+public interface ProductRepo extends JpaRepository<Product, Long>{
 
 }
